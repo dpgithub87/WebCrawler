def68561a21bc2f0a09bf5d762ef50f5b2ddaa8e
Dhandapani Rajeswaran <dhandapani.rajeswaran@truelayer.com>
2024-09-09 10:25:08 +0100

    test: remove same domain validation check

--- WebCrawler.Domain.UnitTests/Validators/UriValidatorTests.cs
+++ WebCrawler.Domain.UnitTests/Validators/UriValidatorTests.cs
@@ -55,16 +55,6 @@ public class UriValidatorTests
         Assert.False(result);
     }
 
-    [Fact(Skip = "This test isn't required if we need to print URIs of different hosts")]
-    public void IsValidUri_ShouldReturnFalse_ForDifferentHost()
-    {
-        var link = "https://anotherdomain.com/resource";
-
-        var result = _uriValidator.IsValidUri(link, _parentUri, out var uri);
-
-        Assert.False(result);
-    }
-
     [Fact]
     public void IsValidUri_ShouldReturnFalse_ForSameAsParentUri()
     {
